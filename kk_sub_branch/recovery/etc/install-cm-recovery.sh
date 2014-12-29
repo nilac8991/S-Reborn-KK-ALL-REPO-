cf05645116ea8e58e430bb22ea141e18fda1a125
nilac8991 <nilac8991@gmail.com>
2014-12-29 22:44:32 +0100

    Merged latest changes made by ADC Team (Release 15)
    - New partitions assignment (Thanks to Christopher)
    - Updated META-INF directory and updater-script for the new partitions assignment

--- kk_sub_branch/recovery/etc/install-cm-recovery.sh
+++ kk_sub_branch/recovery/etc/install-cm-recovery.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 5742592 4d2b35687097a92680d58e5f468c13bda5d6ac6b 3502080 514c963f2f30a9fad91e942fc5d321a0d7fd84ab
+  exec sh /system/etc/recovery-transform.sh 6381568 257d45e1301b8a3445b1562b55367c919f5ccc02 3528704 19195c62cb6491255ae2b18b7838fb3550d45efa
 fi
 
-if ! applypatch -c EMMC:/dev/block/mmcblk0p13:5742592:4d2b35687097a92680d58e5f468c13bda5d6ac6b; then
+if ! applypatch -c EMMC:/dev/block/mmcblk0p13:6381568:257d45e1301b8a3445b1562b55367c919f5ccc02; then
   log -t recovery "Installing new recovery image"
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p8:3502080:514c963f2f30a9fad91e942fc5d321a0d7fd84ab EMMC:/dev/block/mmcblk0p13 4d2b35687097a92680d58e5f468c13bda5d6ac6b 5742592 514c963f2f30a9fad91e942fc5d321a0d7fd84ab:/system/recovery-from-boot.p
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/mmcblk0p8:3528704:19195c62cb6491255ae2b18b7838fb3550d45efa EMMC:/dev/block/mmcblk0p13 257d45e1301b8a3445b1562b55367c919f5ccc02 6381568 19195c62cb6491255ae2b18b7838fb3550d45efa:/system/recovery-from-boot.p
 else
   log -t recovery "Recovery image already installed"
 fi
